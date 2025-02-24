7aa47d9102c4e75567465c4f13b4b4e6c6b2d359
nestorsdelgado <nestorsdelgado@gmail.com>
2025-02-24 15:31:22 +0000

    Cohorts Project
    Co-authored-by: Gara Luzardo <garaluzardo@users.noreply.github.com>
    Co-authored-by: samuglez <samuglez@users.noreply.github.com>
    Co-authored-by: Sebna Omar <Kyomar97@users.noreply.github.com>

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cohorts = require('./cohorts.json')
+const students = require('./students.json')
+const cors = require('cors')
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -11,7 +14,6 @@ const PORT = 5005;
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
-
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
@@ -20,7 +22,7 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(cors({origin: ['localhost:5173']}));
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
@@ -29,6 +31,11 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get('/api/cohorts', (req, res, next) =>
+  res.json(cohorts))
+
+app.get('/api/students', (req, res, next) =>
+  res.json(students))
 
 // START SERVER
 app.listen(PORT, () => {
